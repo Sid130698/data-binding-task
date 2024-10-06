f4dddaca705bdb267b02cc6987769de693ed6c02
Sid130698 <sidsingh130698@gmail.com>
2024-10-07 00:11:18 +0530

    created an example of data binding in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'data-binding-task';
+  isButtonDisabled:boolean = false;
+  textToBeDisplayed:string = "";
+  someOneIsTyping(event:any){
+    this.isButtonDisabled=false;
+    this.textToBeDisplayed = event.target.value;
+  }
+  buttonGotClicked(event:any){
+    if(this.textToBeDisplayed === "submitted" ){
+      this.isButtonDisabled=true;
+   }
+  }
 }
